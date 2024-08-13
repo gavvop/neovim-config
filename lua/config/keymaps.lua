d3d3866f3afbed572de9b5491a79ec141d0995f7
Paolo Carnino <paolo@paolocarnino.com>
2024-08-13 15:25:04 +0200

    Init config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set('n', '<C-M-Left>', '<C-o>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-f>', '/', { noremap = true, silent = false })
+vim.keymap.set('n', '<C-M-Right>', '<C-i>', { noremap = true, silent = true })
+
